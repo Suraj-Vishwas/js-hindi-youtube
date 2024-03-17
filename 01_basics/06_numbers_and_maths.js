6807c924a0861c42bb78bc1f6706187ec6f39cc6
Suraj-Vishwas <163645491+Suraj-Vishwas@users.noreply.github.com>
2024-03-17 06:05:52 +0000

    math.num

---
+++ 01_basics/06_numbers_and_maths.js
@@ -0,0 +1,41 @@
+//  const score = 400;
+// console.log(score);
+
+// const newScore = new Number(400);
+// console.log(newScore);
+
+
+// console.log(newScore.toString().length);
+// console.log(newScore.toFixed(2));
+
+
+// const otherNumber = 13.894;
+// console.log(otherNumber.toPrecision(2));
+
+
+// const hundreds = 100000000;
+// console.log(hundreds.toLocaleString());
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+
+
+// +++++++++++++++ Maths ++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-88));
+// console.log(Math.round(5.4));
+// console.log(Math.round(5.6));
+// console.log(Math.ceil(5.2));
+// console.log(Math.floor(5.9));
+
+
+// console.log(Math.random()); // it values lies b/w 0 and 1 but we can manipulate it
+// console.log((Math.random() * 10) + 1);
+// console.log(Math.floor(Math.random() * 10) + 1);
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
