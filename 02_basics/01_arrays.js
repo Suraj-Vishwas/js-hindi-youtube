ccb454c5f5ed95c881838339721216036fcd5c8f
Suraj-Vishwas <163645491+Suraj-Vishwas@users.noreply.github.com>
2024-03-17 07:52:20 +0000

    array

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,30 @@
+//  array 
+// for study
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
+
+const myArr = [0,1,2,3,4,5,6];
+const myHeros = ["Hulk", "Thor","Wanda"];
+
+const myArr2 = new Array(1,2,3,4)
+// console.log(myArr[0]);
+// console.log(myArr);
+
+
+
+// Array methods
+
+// myArr.push(9);
+// myArr.push(10)
+// myArr.pop()
+
+// myArr.unshift(9);
+// myArr.unshift(67);
+// myArr.shift();
+// myArr.shift()
+// myArr.shift()
+// myArr.shift()
+
+
+
+console.log(myArr);
+
