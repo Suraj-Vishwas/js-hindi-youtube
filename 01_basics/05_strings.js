3f191e7b10c3c6b84e2fa640e7efc2b14aad1727
Suraj-Vishwas <163645491+Suraj-Vishwas@users.noreply.github.com>
2024-03-17 05:34:34 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,43 @@
+const name = "Suraj"; //string diclaration 1's way
+const repoCount = 50;
+
+// console.log(name + repoCount + " Value");
+
+
+// ` ` => back ticks 
+// here we can use also method like (name.toUpperCase)
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+
+// const gameName = new String('Suraj-Vishwas-is-a-programmer');  //string diclaration 2's way
+
+// console.log(gameName[0])
+// console.log(gameName.length);
+// console.log(gameName.__proto__);
+// console.log(gameName.toUpperCase())
+// console.log(gameName.charAt(4))
+// console.log(gameName.indexOf('r'))
+
+
+// const newString = gameName.substring(1,5);
+// console.log(newString)
+
+// const anotherString = gameName.slice(-4,4);
+// console.log(anotherString);
+
+// const newStringOne = "  suraj    vishwas  ";
+// console.log(newStringOne)
+// console.log(newStringOne.trim())
+
+
+
+// const url = "https://hitesh.com/hitesh%20choudhary"
+// console.log(url.replace("%20", '_'));
+
+// console.log(url.includes('hitesh'));
+
+// console.log(gameName.split('-'))
+
+
+
+
--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -2,36 +2,69 @@
 
 // 7 types : String, Number, Boolean, null, undefined, symbol, BigInt
 
-const score = 100
-const scoreValue = 100.3
+// const score = 100
+// const scoreValue = 100.3
 
-const isLoggedIn = false;
-const outSideTemp = null;
-let userEmail;
+// const isLoggedIn = false;
+// const outSideTemp = null;
+// let userEmail;
 
-const id = Symbol('123');
-const anotherId = Symbol('123');
+// const id = Symbol('123');
+// const anotherId = Symbol('123');
 
-console.log(id === anotherId);  //false
+// console.log(id === anotherId);  //false
 
-const bigNumber = 35463463434134134n
+// const bigNumber = 35463463434134134n
+
+
 
 
 // Reference (Non primitive)
 // Array ,Objects, Functions
 
-const heros = ["shaktimam", "naagraj", "doga"];
-let myObj = {
-    name : "suraj",
-    age : 21,
-}
+// const heros = ["shaktimam", "naagraj", "doga"];
+// let myObj = {
+//     name : "suraj",
+//     age : 21,
+// }
 
-const myFunction = function(){
-    console.log("Hello my name is Suraj");
-}
+// const myFunction = function(){
+//     console.log("Hello my name is Suraj");
+// }
 
-console.log(typeof myFunction);
+// console.log(typeof myFunction);
 
 /* for study
 https://262.ecma-international.org/5.1/#sec-11.4.3
 */
+
+
+
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack (Primitive) , Heap (Non-Primitive)
+
+// let myYoutubeName = "SurajVishwas";
+
+// let anotherName = myYoutubeName;
+// anotherName = "VishwasSuraj";
+
+// console.log(myYoutubeName);
+// console.log(anotherName);
+
+
+// let userOne = {
+//     email : "vishwassuraj165@gmail.com",
+//     mobile : 7828116865
+// }
+
+// let userTwo = userOne;
+
+// userTwo.email = "sudhamvishwas@gmail.com";
+// console.log(userOne.email);
+// console.log(userTwo.email);
+
+
+
