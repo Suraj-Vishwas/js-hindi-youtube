93c358c70b2b7ad4650ca24b93ac9bac54d90867
Suraj-Vishwas <163645491+Suraj-Vishwas@users.noreply.github.com>
2024-03-17 03:09:28 +0000

    COMPARISON

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,31 +1,88 @@
-let score = undefined;
+// let score = undefined;
 
 
-// console.log(typeof score);
-// console.log(typeof (score));
+// // console.log(typeof score);
+// // console.log(typeof (score));
 
 
-let valueInNumber = Number(score)
-// console.log(valueInNumber);  
+// let valueInNumber = Number(score)
+// // console.log(valueInNumber);  
 
 
 
-// "33" => 33
-// "33abc" => NaN
-// true => 1; false => 0
+// // "33" => 33
+// // "33abc" => NaN
+// // true => 1; false => 0
 
 
-let isLoggedIn = "jgd";
+// let isLoggedIn = "jgd";
 
-let booleanIsLoggedIn = Boolean(isLoggedIn);
-// console.log(booleanIsLoggedIn);
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// // console.log(booleanIsLoggedIn);
 
-//  1 => true; 0 => false
-// " => false
-// "Suraj" => true
+// //  1 => true; 0 => false
+// // " => false
+// // "Suraj" => true
 
 
-let someNumber = 33;
+// let someNumber = 33;
 
-let stringNumber = String(someNumber);
-console.log(typeof stringNumber);
+// let stringNumber = String(someNumber);
+// console.log(typeof stringNumber);
+
+
+// // ***************************Operations******************//
+
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2**5)
+
+
+// console.log(1+2);
+// console.log("1"+"2");
+// console.log(1+"2");
+// console.log("1"+2);
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+// console.log("1"+3+"2");
+// console.log("1"+"2"+3);
+ 
+// console.log(true);
+// console.log(+true);
+// console.log(+"");
+
+
+// let num1, num2, num3;
+// // this is not appropriate in company
+// num1 = num2 = num3 = 2 + 2;
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+
+// console.log(Number(10>2)); //comparison and type conversion
+
+// console.log("2">1); //avoid this  
+// console.log("02">1);
+
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+/* 
+The reason is that an equality check == and comparison
+> < >= <= work differently.
+Comparisons convert null to a number, treating it as 0.
+That's why (3) null>=0 is true and (1) null>0 is false
+*/
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+
+
+// strict check ===
+// console.log("2" === 2);
+
+
+
